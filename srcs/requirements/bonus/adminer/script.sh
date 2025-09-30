a911a4c47a2f5c7ccb391762972ea54a7d0dc072
Adil Mabrouk <amabrouk@e3r7p11.1337.ma>
2025-09-30 21:27:04 +0100

    adminer

--- srcs/requirements/bonus/adminer/script.sh
+++ srcs/requirements/bonus/adminer/script.sh
@@ -6,6 +6,6 @@ mkdir -p /run/php /var/www/html/adminer
 wget "https://www.adminer.org/latest.php" -O /var/www/html/adminer/adminer.php
 wget "https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css" -O /var/www/html/adminer/adminer.css
 
-sed -i 's/^listen = .*/listen = 8080/' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's/^listen = .*/listen = 8090/' /etc/php/8.2/fpm/pool.d/www.conf
 
-/usr/sbin/php-fpm7.4 -F
+/usr/sbin/php-fpm8.2 -F
